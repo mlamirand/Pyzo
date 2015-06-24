6029a6acd525bd7785d5f6b9846a9d89f1925a73
Monica Lamirand <lamirandm16@hanover.edu>
2015-06-24 13:21:07 -0700

    Create pyzo repo

---
+++ data.py
@@ -0,0 +1,45 @@
+import numpy
+
+#first donor
+data1 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (1).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
+
+#second donor
+data2 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (2).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
+
+#third donor
+data3 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (3).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
+    
+#fourth donor
+data4 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (4).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
+    
+    
+#fifth donor
+data5 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (5).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
+    
+    
+#sixth donor
+data6 = numpy.genfromtxt(
+    fname = 'C:\\Users\\Monica\\Desktop\\REU - UW\\Datasets\\MicroarrayExpression (6).csv',
+    delimiter = ',',
+    invalid_raise = False,
+    )
