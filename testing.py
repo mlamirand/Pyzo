aa75799ea07ca0b4a8cfe5b7fbfa5c6c8c266ce1
Monica Lamirand <lamirandm16@hanover.edu>
2015-07-22 16:27:40 -0700

    just tests

--- testing.py
+++ testing.py
@@ -1,55 +1,72 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 import random
+import matplotlib.pyplot as plt
 
-class eGreedy:
-    def __init__(self, epsilon, counts, Qk):
-        self.epsilon = epsilon
-        self.counts = counts
-        self.Qk = Qk
-        
-    def initialize(self, n_arms):
-        self.counts = [0 for col in range(n_arms)]
-        self.Qk = [0 for col in range(n_arms)]
-        return
-        
-    def choose_arm(self):
-        #returns an arm for testing
-        if np.random.random() > self.epsilon:
-            #eploit
-            return np.argmax(self.Qk)   #returns index of largest Qt(a)
-        else:
-            #explore
-            return np.random.randint(len(self.Qk))    #returns random arm
+def reward(x1, x2, x3):
+    noise = np.random.normal(0, 1)
+    y = x1 + x2 - 0.5 * x3 + noise
     
-    def update(self, chosen_arm, reward):
-        self.counts[chosen_arm] += 1
-        n = self.counts[chosen_arm]
+    if y < 5 or y > 7:
+        prob = 0
+    else:
+        prob = 0.5
         
-        Qk = self.Qk[chosen_arm]
-        new_Qk = Qk + (1 /n) * (reward - Qk)
-        self.Qk[chosen_arm] = new_Qk
-        return
+    z = random.random()
+    if z < prob:
+        return 1
+    else:
+        return 0
+    
+class brain:
+    def __init__(self, x1, x2, x3, values, chosen, epsilon):
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+        self.values = [[[0 for k in range(11)] for j in range(11)] for i in range(11)]
+        self.chosen = [[[0 for k in range(11)] for j in range(11)] for i in range(11)]
+        self.epsilon = epsilon
         
+    def choose_xs(self):
+        if np.random.random() > self.epsilon:
+            n = np.argmax(self.values)
+            [x, y, z] = np.unravel_index([n], (11, 11, 11))
+            x1 = x[0]
+            x2 = y[0]
+            x3 = z[0]
+            return [x1, x2, x3]
+        else:
+            x1 = random.randint(0, 10)
+            x2 = random.randint(0, 10)
+            x3 = random.randint(0, 10)
+            return [x1, x2, x3]
+            
+    def update(self):
+        [x1, x2, x3] = self.choose_xs()
+        r = reward(x1, x2, x3)
+        
+        self.chosen[x1][x2][x3] += 1
+        n = self.chosen[x1][x2][x3]
+        self.values[x1][x2][x3] = self.values[x1][x2][x3] + (1/n) * (r - self.values[x1][x2][x3])
+        
+        return r
+
+
 def main():
-    players = [eGreedy(0.1, [], []) for i in range(2000)]
-    for j in range(0, 2000):
-        players[j].initialize(10)
+    
+    tests = [brain(0, 0, 0, [], [], 0.1) for i in range(1000)]
     
     averages = []
-    for i in range(0, 1000):
+    for i in range(0, 2000):
         rewards = []
-        for player in players:
-            arm = player.choose_arm()
-            reward = np.random.normal(player.Qk[arm], 1)
-            player.update(arm, reward)
+        for test in tests:
+            reward = test.update()
             rewards.append(reward)
-        averages.append(np.sum(rewards)/2000)
+        averages.append(np.sum(rewards)/1000)
         
-    x = np.arange(0, 1000, 1)
+
+    x = np.arange(0, 2000, 1)
     y = np.array(averages)
-    plt.scatter(x, y, color = (0.75, 0, 1.0))
-  
-  
+    plt.scatter(x, y, color = (0.9, 0, 0))
+    
 main()
+    
--- testing2.py
+++ testing2.py
@@ -1,46 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import math
 import random
 
-def categorical_draw(probs):
-  z = random.random()
-  cum_prob = 0.0
-  for i in range(len(probs)):
-    prob = probs[i]
-    cum_prob += prob
-    if cum_prob > z:
-      return i
-  
-  return len(probs) - 1
-
-class Softmax:
-  def __init__(self, temperature, counts, Qk):
-    self.temperature = temperature
-    self.counts = counts
-    self.Qk = Qk
-    return
-  
-  def initialize(self, n_arms):
-    self.counts = [0 for col in range(n_arms)]
-    self.Qk = [0.0 for col in range(n_arms)]
-    return
-  
-  def select_arm(self):
-    z = sum([math.exp(v / self.temperature) for v in self.Qk])
-    probs = [math.exp(v / self.temperature) / z for v in self.Qk]
-    return categorical_draw(probs)
-
-  def update(self, chosen_arm, reward):
-    self.counts[chosen_arm] += 1
-    n = self.counts[chosen_arm]
+class eGreedy:
+    def __init__(self, epsilon, counts, Qk):
+        self.epsilon = epsilon
+        self.counts = counts
+        self.Qk = Qk
+        
+    def initialize(self, n_arms):
+        self.counts = [0 for col in range(n_arms)]
+        self.Qk = [np.random.standard_normal() for col in range(n_arms)]
+        return
+        
+    def choose_arm(self):
+        if np.random.random() > self.epsilon:
+            return np.argmax(self.Qk) 
+        else:
+            return np.random.randint(len(self.Qk)) 
     
-    Qk = self.Qk[chosen_arm]
-    new_Qk = Qk + (1 /n) * (reward - Qk)
-    self.Qk[chosen_arm] = new_Qk
-    return
-
+    def update(self, chosen_arm, reward):
+        self.counts[chosen_arm] += 1
+        n = self.counts[chosen_arm]
+        
+        Qk = self.Qk[chosen_arm]
+        new_Qk = Qk + (1 /n) * (reward - Qk)
+        self.Qk[chosen_arm] = new_Qk
+        return
+        
 def main():
-  
-    players = [Softmax(0.1, [], []) for i in range(2000)]
+    
+    players = [eGreedy(0.1, [], []) for i in range(2000)]
     for j in range(0, 2000):
         players[j].initialize(10)
     
@@ -48,7 +39,7 @@ def main():
     for i in range(0, 1000):
         rewards = []
         for player in players:
-            arm = player.select_arm()
+            arm = player.choose_arm()
             reward = np.random.normal(player.Qk[arm], 1)
             player.update(arm, reward)
             rewards.append(reward)
@@ -56,7 +47,7 @@ def main():
         
     x = np.arange(0, 1000, 1)
     y = np.array(averages)
-    plt.scatter(x, y, color = (0, 0.8, 0.9))
+    plt.scatter(x, y, color = (0, 1.0, 0))
   
   
 main()
