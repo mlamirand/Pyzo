a7eec57d0ab0731c84d6b048cc794033b0425ae4
Monica Lamirand <lamirandm16@hanover.edu>
2015-07-30 17:54:05 -0700

    tests for project

--- testing.py
+++ testing.py
@@ -2,11 +2,12 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-def reward(x1, x2, x3):
+
+def reward(a1, a2, a3):
     noise = np.random.normal(0, 1)
-    y = x1 + x2 - 0.5 * x3 + noise
+    y = a1 + a2 - a3 + noise
     
-    if y < 5 or y > 7:
+    if y < 2 or y > 5:
         prob = 0
     else:
         prob = 0.5
@@ -16,57 +17,53 @@ def reward(x1, x2, x3):
         return 1
     else:
         return 0
+        
+        
+def generate_values():
+    combos = []
+    for i in range(11):
+        for j in range(11):
+            for k in range(11):
+                s = (i, j, k)
+                combos.append(s)
+                
+    y_vals = []
+    for s in combos:
+        noise = np.random.normal(0, 1)
+        y = s[0] + s[1] - s[2] + noise
+        y_vals.append(y)
     
-class brain:
-    def __init__(self, x1, x2, x3, values, chosen, epsilon):
-        self.x1 = x1
-        self.x2 = x2
-        self.x3 = x3
-        self.values = [[[0 for k in range(11)] for j in range(11)] for i in range(11)]
-        self.chosen = [[[0 for k in range(11)] for j in range(11)] for i in range(11)]
-        self.epsilon = epsilon
-        
-    def choose_xs(self):
-        if np.random.random() > self.epsilon:
-            n = np.argmax(self.values)
-            [x, y, z] = np.unravel_index([n], (11, 11, 11))
-            x1 = x[0]
-            x2 = y[0]
-            x3 = z[0]
-            return [x1, x2, x3]
-        else:
-            x1 = random.randint(0, 10)
-            x2 = random.randint(0, 10)
-            x3 = random.randint(0, 10)
-            return [x1, x2, x3]
+    return (combos, y_vals)
+    
+def find_indices(y_vals):
+    
+    indices = []
+    for i in range(len(y_vals)):
+        if y_vals[i] > 2 and y_vals[i] < 5:
+            indices.append(i)
             
-    def update(self):
-        [x1, x2, x3] = self.choose_xs()
-        r = reward(x1, x2, x3)
-        
-        self.chosen[x1][x2][x3] += 1
-        n = self.chosen[x1][x2][x3]
-        self.values[x1][x2][x3] = self.values[x1][x2][x3] + (1/n) * (r - self.values[x1][x2][x3])
-        
-        return r
+    return indices
 
+def find_best_xs():
+    
+    combos, y_vals = generate_values()
+    
+    indices = find_indices(y_vals)
+    x_value_triplets = []
+    for index in indices:
+        x_value_triplets.append(combos[index])
+    return x_value_triplets
+    
+    
+def choose_triplet():
+    triplets = find_best_xs()
+    triplet = random.choice(triplets)
+    return triplet
 
+            
+    
 def main():
     
-    tests = [brain(0, 0, 0, [], [], 0.1) for i in range(1000)]
-    
-    averages = []
-    for i in range(0, 2000):
-        rewards = []
-        for test in tests:
-            reward = test.update()
-            rewards.append(reward)
-        averages.append(np.sum(rewards)/1000)
-        
-
-    x = np.arange(0, 2000, 1)
-    y = np.array(averages)
-    plt.scatter(x, y, color = (0.9, 0, 0))
     
 main()
     
--- testing2.py
+++ testing2.py
@@ -1,53 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 import random
+import matplotlib.pyplot as plt
+import time
 
-class eGreedy:
-    def __init__(self, epsilon, counts, Qk):
-        self.epsilon = epsilon
-        self.counts = counts
-        self.Qk = Qk
-        
-    def initialize(self, n_arms):
-        self.counts = [0 for col in range(n_arms)]
-        self.Qk = [np.random.standard_normal() for col in range(n_arms)]
-        return
-        
-    def choose_arm(self):
-        if np.random.random() > self.epsilon:
-            return np.argmax(self.Qk) 
-        else:
-            return np.random.randint(len(self.Qk)) 
+def reward(x1, x2, x3):
+    noise = np.random.normal(0, 1)
+    y = x1 + x2 - 0.5 * x3 + noise
     
-    def update(self, chosen_arm, reward):
-        self.counts[chosen_arm] += 1
-        n = self.counts[chosen_arm]
+    if y < 2 or y > 5:
+        prob = 0
+    else:
+        prob = 1
         
-        Qk = self.Qk[chosen_arm]
-        new_Qk = Qk + (1 /n) * (reward - Qk)
-        self.Qk[chosen_arm] = new_Qk
-        return
-        
+    z = random.random()
+    if z < prob:
+        return 1
+    else:
+        return 0
+    
 def main():
     
-    players = [eGreedy(0.1, [], []) for i in range(2000)]
-    for j in range(0, 2000):
-        players[j].initialize(10)
-    
-    averages = []
-    for i in range(0, 1000):
-        rewards = []
-        for player in players:
-            arm = player.choose_arm()
-            reward = np.random.normal(player.Qk[arm], 1)
-            player.update(arm, reward)
-            rewards.append(reward)
-        averages.append(np.sum(rewards)/2000)
+    for i in range(100):
+        print(reward(2, 2, 2))
         
-    x = np.arange(0, 1000, 1)
-    y = np.array(averages)
-    plt.scatter(x, y, color = (0, 1.0, 0))
-  
-  
 main()
